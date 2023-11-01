c3a0194a99b4bff374cc0d01fb84883f7933c850
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-01 17:47:46 +0000

    add items to cart

--- checkout.dart
+++ checkout.dart
@@ -25,10 +25,39 @@ class Item {
   final int quantity;
 
   double get price => quantity * product.price;
+
+  @override
+  String toString() => "$quantity x ${product.name}: \$$price";
 }
 
 class Cart {
-  // TODO: Items Collection
+  final Map<int, Item> _items = {};
+
+  void addProduct(Product product) {
+    final item = _items[product.id];
+
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _items.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  @override
+  String toString() {
+    if (_items.isEmpty) {
+      return "Cart is Empty";
+    }
+
+    final itemizedList =
+        _items.values.map((item) => item.toString()).join("\n");
+
+    return "------\n$itemizedList\nTotal: \$${total()}\n------";
+  }
 }
 
 const allProducts = <Product>[
@@ -41,6 +70,8 @@ const allProducts = <Product>[
 ];
 
 void main() {
+  final cart = Cart();
+
   while (true) {
     stdout.write(
       "What do you want to do? (v)iew items, (a)dd item, (c)heckout: ",
@@ -50,10 +81,11 @@ void main() {
     if (line == "a") {
       final product = chooseProduct();
       if (product != null) {
-        print(product.displayName);
+        cart.addProduct(product);
+        print(cart);
       }
     } else if (line == "v") {
-      // TODO: Implement
+      print(cart);
     } else if (line == "c") {
       // TODO: Implement
     }
